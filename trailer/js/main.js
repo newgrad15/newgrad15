baf59211b1277b8d3622587e326e77ebd3e4728f
fujii-kotaro <fujii-kotaro@KYCNT1502-08.local>
2015-04-12 20:50:48 +0900

    新しいサイトの作成

---
+++ trailer/js/main.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    new TimeCounter( "time_counter", 2015, 04, 17, 10, 00, 00 );
+    new OpeningAnimation( "canvas" );
+}
+
---
+++ trailer/js/opening_animation.js
@@ -0,0 +1,102 @@
+var OpeningAnimation = function( id ){
+    this.Reset = function( id ){
+	this.element = document.getElementById( id );
+	this.ctx = this.element.getContext( '2d' );
+	this.xCharNum = 16 + 2;
+	this.yCharNum = 16 + 2;
+	this.OnResize();
+	this.ClearCanvas("black");
+	
+	this.fontSize = this.GetFontSize();
+	this.scale = 1.0
+
+	this.string  = "\nカヤック15年新卒20名は・・・\n\n"
+	this.string += "数年前この20名で起業をし、\n\n"
+	this.string += "会社経営をしていた\n\n"
+	this.string += "\n\n"
+	this.string += "だが、社内である事件が起こり\n\n"
+	this.string += "会社を倒産させてしまった\n\n"
+	this.string += "\n\n\n"
+	this.string += "そこで、何がおきたのか・・・\n"
+	
+	this.draw_x = this.GetInitDrawX();
+	this.draw_y = this.GetInitDrawY();
+	this.Loop( 0, 0 )
+    }
+    this.GetFontSize = function(){
+	x_fontSize = Math.floor( ( this.element.height ) / ( this.xCharNum ) );
+	y_fontSize = Math.floor( ( this.element.width  ) / ( this.yCharNum ) );
+	if( x_fontSize < y_fontSize ){
+	    console.log("x")
+	    return x_fontSize;
+	}else{
+	    console.log("y")
+	    return y_fontSize;
+	}
+    }
+    this.GetInitDrawX = function(){
+	console.log( this.fontSize * this.xCharNum )
+	var i = this.element.width - ( this.element.width - ( this.fontSize * this.xCharNum  ) ) / 2  - this.fontSize;
+	console.log(i)
+	return i
+    }
+    this.GetInitDrawY = function(){
+	return ( this.element.height - this.yCharNum * this.fontSize ) / 2 + this.fontSize * 2;
+    }
+    this.OnResize = function(){
+	this.element.width  =  window.innerWidth;
+	this.element.height = window.innerHeight;
+    }
+
+    this.ClearCanvas = function( color ){
+	this.ctx.fillStyle = color;
+	var w = this.element.width;
+	var h = this.element.height;
+
+	var grad5 = this.ctx.createRadialGradient( w/2, h/2, w/6, w/2, h/2, w/2 );
+	grad5.addColorStop(0,'#000000');
+	grad5.addColorStop(1.0,'#222222');
+
+	this.ctx.beginPath();
+	this.ctx.fillStyle = grad5;
+	this.ctx.fill();
+
+	this.ctx.fillRect( 0, 0, this.element.width, this.element.height );
+    }
+    
+    this.Loop = function( frame, time ){
+	var that = this;
+
+//	that.ctx.shadowBlur = 1000;
+//	that.ctx.shadowColor = "blue";
+	
+	that.ctx.font = "bold #fontSizepx 'メイリオ'".replace("#fontSize",that.fontSize);
+	
+	var ch = ""
+	if( frame < that.string.length ){
+	    ch = that.string[ frame ]
+	}
+	
+	if(ch != ""){
+	    that.ctx.shadowBlur = this.fontSize / 2;
+	    that.ctx.shadowColor = "gray";
+	    that.ctx.fillStyle = "red";
+	    that.ctx.fillText( that.string[ frame ], that.draw_x - 4, that.draw_y );
+	    that.ctx.fillStyle = "cyan";
+	    that.ctx.fillText( that.string[ frame ], that.draw_x + 4, that.draw_y );	
+	    that.ctx.fillStyle = "white";
+	    that.ctx.fillText( that.string[ frame ], that.draw_x, that.draw_y );
+	    
+	    that.draw_y += this.fontSize;
+	    if( ch == "\n" ){
+		that.draw_x -= this.fontSize;
+		that.draw_y = this.GetInitDrawY();
+	    }
+	    nextFrame = frame + 1
+	    nextTime = time + 1
+	    setTimeout( function(){ that.Loop( nextFrame, nextTime ) }, 200 );
+	}
+    }
+
+    this.Reset( id )
+}
